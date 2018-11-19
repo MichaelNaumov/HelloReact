40d2648a861a72802c0f1d2640bd89dd8f61cc7f
Michael Naumov <mihail.naumov23@gmail.com>
2018-11-19 13:32:25 +0200

    data and component move

---
+++ app/components/data.js
@@ -0,0 +1,7 @@
+const data = [
+  { id: 1, name: "Michael Naumov", email: "mihail.naumov23@gmail.com"},
+  { id: 2, name: "Garry Numan", email: "gary_numa3@example.com"},
+  { id: 3, name: "Ariana Grante", email: "ari_grani@example.com"},
+];
+
+export default data;
