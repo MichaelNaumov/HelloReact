3b3a0a2ffb4aa088e9112d71aa109d05f1287d81
Michael Naumov <mihail.naumov23@gmail.com>
2018-11-19 13:32:41 +0200

    imports and render contact

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -1,12 +1,14 @@
-import React from "react"
+import React from "react";
+import Contact from "./Contact";
+import data from './data';
 
 class Layout extends React.Component {
   render() {
     return  (
       <div id='Layout' className='pure-g'>
-        <div className="pure-u-1-3"><p>First</p></div>
-        <div className="pure-u-1-3"><p>Second</p></div>
-        <div className="pure-u-1-3"><p>Third</p></div>
+        { data.map(info =>
+          <Contact key={info.id} {...info} />
+        )}
       </div>
     )
   }
